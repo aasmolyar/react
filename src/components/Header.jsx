9cb7ea071d98adcd0643439272cc2f20087ca8cb
Andrey Smolyar <smolyar2022@internet.ru>
2023-05-20 07:27:01 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+    <img src="https://avatars.mds.yandex.net/i?id=11ab1bd7972e70aaef3790a519e60886ad31fc33-7716203-images-thumbs&n=13" alt="logo" />
+  </header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+    <img src="https://t-toner.ru/wa-data/public/site/data/contact_us.jpg" alt="Mgn picture" />
+    </div>
+    <div>
+      avatar + description
+      </div>
+    <div>
+      My posts
+      <div>
+        New post
+      </div>
+      <div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile
