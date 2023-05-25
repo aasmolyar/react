d6ede5e9cd2d1b903c61eb58bb29118d5c666a3f
aasmolyar <110403346+aasmolyar@users.noreply.github.com>
2023-05-25 21:01:13 +0500

    add Dialogs module

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import classes from './Profile.module.css';
+import s from './Profile.module.css';
 import MyPosts from './Profile/MyPosts/MyPosts';
 
 const Profile = () => {
     return (
-      <div className={classes.content}>
+      <div>
     <div>
     <img src="https://t-toner.ru/wa-data/public/site/data/contact_us.jpg" alt="main" />
     </div>
