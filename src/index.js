0f677e0c9da6863a963eb6b697a587861775452c
aasmolyar <110403346+aasmolyar@users.noreply.github.com>
2023-07-31 21:34:29 +0500

    add mapStateToProps and mapDispatchToProps

--- src/index.js
+++ src/index.js
@@ -5,27 +5,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./components/redux/redux-store";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-let _callSubscriber = (state) => {
-  root.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App
-          state={state}
-          dispatch={store.dispatch.bind(store)}
-          store={store}
-        />
-      </BrowserRouter>
-    </React.StrictMode>
-  );
-  reportWebVitals();
-};
-
-_callSubscriber(store.getState());
-
-store.subscribe(() => {
-  let state = store.getState();
-  _callSubscriber(state);
-});
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+reportWebVitals();
