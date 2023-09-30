1d29a94cda300c1307c2d80002ccfb421f4b9aad
aasmolyar <110403346+aasmolyar@users.noreply.github.com>
2023-09-30 14:18:18 +0500

    add login

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
+import HeaderContainer from "../src/components/Header/HeaderContainer";
 
 import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import UsersContainer from "./components/Users/UsersContainer";
@@ -14,7 +15,7 @@ import UsersContainer from "./components/Users/UsersContainer";
 const App = (props) => {
   return (
     <div className="app-wrapper">
-      <Header />
+      <HeaderContainer />
       <Navbar />
       <div className="app-wrapper-content">
         {
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import classes from './Header.module.css';
+import s from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Header = (props) => {
     return (
-      <header className={classes.header}>
+      <header className={s.header}>
         <img src="https://avatars.mds.yandex.net/i?id=11ab1bd7972e70aaef3790a519e60886ad31fc33-7716203-images-thumbs&n=13" alt="logo" />
+        <div className={s.loginBlock}>
+          {props.isAuth ? props.login
+            : <NavLink to={'/login'}>Login</NavLink>}
+        </div>
     </header>
     ) 
 }
